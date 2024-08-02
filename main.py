e08d7647af4845be6bc7ada571badee3b7eabcca
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-08-02 13:32:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,40 @@
 # Current date
 # String Concatenation in Python
 
+# Example 1
+
+phrase1 = "Hello, everyone! How "
+
+phrase2 = "are you doing today?"
+
+full_sentence = phrase1 + phrase2
+
+print(full_sentence)
+
+
+# Example 2
+# Assigning a string value to each variable
+
+first_name = "Mike"
+last_name = "Jenkins"
+
+# Can also use single quotes to assign a string to
+# a variable
+
+# first_name = 'Mike'
+# last_name = 'Jenkins'
+
+# Concatenation means 'to join'
+
+# Use the concatenation operator to join two strings
+# and a blank space
+
+# Remember to add a blank space to make output
+# look nice
+
+fullName = first_name + last_name
+
+# fullName = first_name + " " + last_name
+
+# Print my full name
+print ("My full name is: " + fullName)
