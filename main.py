11dd53efaee5ce5b897d302bb44637f86d8ace53
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-08-02 13:48:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 # Student name
 # Current date
 # String Concatenation
+
+# Part 1
+# Use concatenation to build and display a string that says which city and state you live in
+
+
+
+
+# Part 2
+# Create a custom message that welcomes the user by first name to a game you created
+# Create one variable to store the user's first name
+# Create another variable that stores the welcome message
+# Use concatenation to create the customized message
+
+
+
+# Part 3
+# Use concatenation to display a sentence that tells us your first name and age
+# Assign a number to your age variable
+# Use the built-in Python str( ) function to convert your age to a string (when doing concatenation)
+
+
+# Part 3
+# Use an f-string to build and display a string that contains your first name, last name, and your height in inches
