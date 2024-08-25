dc81dca678c001281e82b6d11b3b6574037b4156
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-08-25 06:46:12 -0400

    Create concat-practice.py

---
+++ concat-practice.py
@@ -0,0 +1,27 @@
+# Concatenation Practice
+
+
+# Work with your assigned partner
+# Each person in your team should write the code to create and display longer strings
+
+
+# Part 1
+# Create variables to store your first, middle, and last name
+# Then have Python write and print a message that contains your concatenated names
+
+
+
+# Part 2
+# Assume you're building a Space Invaders game
+# Use concatenation to create and display a custom message to the user that includes his/her first name
+
+
+
+# Part 3
+# Use concatenation to build and display a string that includes your first name, last name, and the year you were born
+
+
+
+# Part 4
+# Use concatenation to create and display a sentence that tells the user which country in the world
+# you would visit if money were no object
